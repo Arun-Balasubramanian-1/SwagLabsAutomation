d99df3a3f316c5c4832ec1d4028741678af4eafa
Arun-Balasubramanian-1 <arun.balasubramanian.97@gmail.com>
2023-12-09 14:24:38 +0530

    annotations

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,16 +1,14 @@
 package testcases;
 
 import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
 import org.testng.Assert;
-import org.testng.annotations.Test;
 
 import pages.SauceDemoLogin;
 import pages.SauceProducts;
 
 public class LoginTest extends BaseClass  {
 	
-	@Test
+	@Test(enabled = true)
 	public void loginFailure() throws InterruptedException {
 		SauceDemoLogin sauceLogin = new SauceDemoLogin(driver);
 		
@@ -18,17 +16,17 @@ public class LoginTest extends BaseClass  {
 		Thread.sleep(4000);
 		String actualErrorMessage = sauceLogin.getLoginFailureMessage();
 		System.out.println(actualErrorMessage);
-		AssertJUnit.assertEquals(actualErrorMessage, "Epic sadface: Username and password do not match any user in this service");
+		Assert.assertEquals(actualErrorMessage, "Epic sadface: Username and password do not match any user in this service");
 	}
 	
-	@Test
+	@Test(enabled = true, groups = {"sanity"})
 	public void loginSuccess()throws InterruptedException {
 		SauceDemoLogin sauceLogin = new SauceDemoLogin(driver);
 		SauceProducts  sauceProduct = new SauceProducts(driver);
 
 		sauceLogin.login("standard_user", "secret_sauce");
 		Thread.sleep(4000);
-		AssertJUnit.assertEquals(sauceProduct.isProductsSectionPresent(), true);
+		Assert.assertEquals(sauceProduct.isProductsSectionPresent(), true);
 	}
 
 }
